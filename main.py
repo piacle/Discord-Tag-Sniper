bc295145e39d7c59570b9dc8bc111daec66c27ee
ghast <111450473+arisuandlainkissingcompetition123456789@users.noreply.github.com>
2022-11-28 06:14:32 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
-import httpx
-import asyncio
-import PyTerm
-
+from asyncio import run, sleep
+from PyTerm import Console
+from httpx import AsyncClient
+from threading import Thread
 class DiscordTagSniper:
     def __init__(self) -> None:
-        self.attempts = 0
+        self.requests, self.ratelimits, self.errors, self.claimed, self.tokens, self.main_token, self.password, self.tag, self.client, self.uid = 0, 0, 0, False, open(input("[^] Path for tha tokens > ")).readlines(), input("[^] Your main token > "), input("[^] Your password > "), input("[^] Their tag > "), AsyncClient(), input("[^] Their ID > ")
+        self.username, self.discriminator = self.tag.split("#")[0], self.tag.split("#")[1]
 
-        self.token = str(input("Discord Token: "))
-        self.password = str(input("Discord Password: "))
-        self.tag = str(input("Target's Discord Tag: "))
-        self.user_id = int(input("Target's Discord User ID: "))
-
-        self.username = self.tag.split("#")[0]
-        self.discriminator = self.tag.split("#")[-1]
-
-        self.client = httpx.AsyncClient()
-        self.client.headers = {"Authorization": self.token} 
+    async def set_title(self):
+        while self.claimed is False:
+            Console.set_title(f"Discord Tag Sniper | Requests: {self.requests} | Ratelimits: {self.ratelimits} | Errors: {self.errors}")
 
     async def claim_user(self):
         r = await self.client.post(
             url = "https://discord.com/api/v9/users/@me",
+            headers = {
+                "Authorization": self.main_token
+            },
             data = {
                 "username": self.username, 
                 "password": self.password, 
@@ -28,31 +25,45 @@ class DiscordTagSniper:
         )
 
         if r.status_code == 200:
-            self.attempts += 1
-            print(f"[+] Sniped Tag: {self.tag} | Attempts: {self.attempts}")
-            PyTerm.Console.set_title(f"Discord Tag Sniper | Attempts: {self.attempts}")
-            print("Press any key to exit...")
-            PyTerm.Console.get_char()
-        elif r.status_code == 400:
-            self.attempts += 1
-            PyTerm.Console.set_title(f"Discord Tag Sniper | Attempts: {self.attempts}")
+            self.requests += 1
+            self.claimed = True
+            print(f"[+] Sniped Tag: {self.tag} | Requests: {self.requests} | Ratelimits: {self.ratelimits} | Errors: {self.errors}")
+        elif r.status_code == 429:
+            self.ratelimits += 1
         else:
-            self.attempts += 1
+            self.errors += 1
             print(f"[-] {r.json()}")
-            PyTerm.Console.set_title(f"Discord Tag Sniper | Attempts: {self.attempts}")
 
 
     async def check_user(self):
-        r = await self.client.get(f"https://discord.com/api/v9/users/{self.user_id}")
-        r = r.json()
-        return (r["username"], r["discriminator"])
-
-    async def start(self):
-        while True:
-            asyncio.sleep(2)
-            info = await self.check_user()
-            if info[0] != self.username or info[1] != self.discriminator:
-                await self.claim_user()
+        while self.claimed is False:
+            for token in self.tokens:
+                r = await self.client.get(
+                    url = f"https://discord.com/api/v9/users/{self.uid}", 
+                    headers = {
+                        "Authorization": token
+                    })
+                self.requests += 1
+                json = r.json()
+                
+                if (r.status_code == 200):
+                    if (json["username"] != self.username or json["discriminator"] != self.discriminator):
+                        await self.claim_user()
+                elif r.status_code == 429:
+                    self.ratelimits += 1
+                    await sleep(json['retry_after'] + .5)
+                else:
+                    self.errors += 1
+                
 
 if __name__ == "__main__":
-    asyncio.run(DiscordTagSniper().start())
+    a = DiscordTagSniper()
+    t = int(input("[^] Threads > "))
+    Console.clear()
+    Thread(target=run, args=[a.set_title()], daemon=True).start()
+    [Thread(target=run, args=[a.check_user()], daemon=True).start() for _ in range(t)]
+    while True:
+        if a.claimed == True:
+            break
+        else:
+            pass
